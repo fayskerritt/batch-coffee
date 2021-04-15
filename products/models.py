9098be6dd849282751b80dd948f5e6372ade704c
fayskerritt <fayskerritt1@gmail.com>
2021-04-15 15:32:26 +0000

    Added products app, Category and Product models

--- products/models.py
+++ products/models.py
@@ -10,3 +10,22 @@ class Category(models.Model):
 
     def get_display_name(self):
         return self.display_name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(
+        'Category', null=True, blank=True, on_delete=models.CASCADE)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    region = models.CharField(max_length=254, null=True, blank=True)
+    variety = models.CharField(max_length=254, null=True, blank=True)
+    altitude = models.CharField(max_length=20, null=True, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(
+        max_digits=6, decimal_places=2, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
