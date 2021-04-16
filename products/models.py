c1134cb55bc8aafe4e0f17186eb653c3f4741ccb
fayskerritt <fayskerritt1@gmail.com>
2021-04-16 14:04:24 +0000

    Started product template

--- products/models.py
+++ products/models.py
@@ -23,16 +23,16 @@ class Product(models.Model):
         ('500g', '500 grams'),
     )
     category = models.ForeignKey(
-        'Category', null=True, blank=True, on_delete=models.CASCADE)
-    sku = models.CharField(max_length=254, null=True, blank=True)
+        'Category', null=True, on_delete=models.CASCADE)
+    sku = models.CharField(max_length=254, null=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
     bag_size = models.CharField(max_length=4, choices=BAG_SIZES)
     region = models.CharField(max_length=254, null=True, blank=True)
-    variety = models.CharField(max_length=254, null=True, blank=True)
+    variety = models.CharField(max_length=254, null=True)
     altitude = models.CharField(max_length=20, null=True, blank=True)
     strength = models.DecimalField(
-        null=True, blank=True, max_digits=2, decimal_places=0, validators=[
+        null=True, max_digits=2, decimal_places=0, validators=[
             MinValueValidator(0), MaxValueValidator(10)])
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(null=True, blank=True)
