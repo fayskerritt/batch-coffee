ae195f331af6f932c134c3cc6debda59d1777c08
fayskerritt <fayskerritt1@gmail.com>
2021-04-20 09:22:19 +0000

    Added sorting functionality to shop-nav in views.py

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
 from .models import Product, Category, Variety
 
 
@@ -14,6 +15,24 @@ def all_products(request):
     query = None
     category = None
     variety = None
+
+    if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+
+            if sortkey == "strength":
+                sortkey = 'lower_strength'
+                products = products.annotate(lower_strength=Lower("strength"))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
 
     if request.GET:
         if 'category' in request.GET and 'variety' not in request.GET:
@@ -47,10 +66,13 @@ def all_products(request):
                     region__icontains=query) | Q(variety__icontains=query)
             products = products.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'products': products,
         'search_term': query,
         'current_category': category,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/shop.html', context)
