91c6de06b24c2f889c3f675db6a1149848bfae50
fayskerritt <fayskerritt1@gmail.com>
2021-04-23 20:19:47 +0000

    Added checkout views and templates

--- checkout/forms.py
+++ checkout/forms.py
@@ -28,7 +28,7 @@ class OrderForm(forms.ModelForm):
             'country': 'Country',
         }
 
-        self.fields['full_name'].widget.attrs['autofocus'] = True
+        self.fields['email'].widget.attrs['autofocus'] = True
         for field in self.fields:
             if self.fields[field].required:
                 placeholder = f'{placeholders[field]} *'
