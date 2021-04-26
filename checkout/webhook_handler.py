f43b7b175a40c1aaa3c33b126e04b6a716e5a7fc
fayskerritt <fayskerritt1@gmail.com>
2021-04-26 23:54:21 +0000

    Added cache checkout view to cache checkout form in payment intent and fixed bug in checkout_success.html(added if >= 50  statement)

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -11,6 +11,7 @@ class StripeWH_Handler:
         """
         Handle a generic, unknown or unexpected webhook event
         """
+
         return HttpResponse(
             content=f'Unhandled webhook received: {event["type"]}',
             status=200)
@@ -19,6 +20,9 @@ class StripeWH_Handler:
         """
         Handle the payment_intent.succeeded webhook from Stripe
         """
+
+        intent = event.data.object
+        print(intent)
         return HttpResponse(
             content=f'Webhook received: {event["type"]}',
             status=200)
@@ -27,6 +31,7 @@ class StripeWH_Handler:
         """
         Handle the payment_intent.payment_failed webhook from Stripe
         """
+
         return HttpResponse(
             content=f'Webhook received: {event["type"]}',
             status=200)
