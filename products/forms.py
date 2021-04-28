bd90c2967f30e01601305f76e2c304f94ad4ea7c
fayskerritt <fayskerritt1@gmail.com>
2021-04-28 23:37:33 +0000

    Added add_product template, urls and view to Product -  store admin

--- products/forms.py
+++ products/forms.py
@@ -11,8 +11,8 @@ class ProductForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         categories = Category.objects.all()
-        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+        display_names = [(c.id, c.get_display_name()) for c in categories]
 
-        self.fields['category'].choices = friendly_names
+        self.fields['category'].choices = display_names
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-black rounded-0'
--- products/views.py
+++ products/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
-from django.http import HttpResponseRedirect
 from django.contrib import messages
 from django.db.models import Q
 from django.db.models.functions import Lower
 
+from .models import Product, Category, Variety
+from .forms import ProductForm
+
 from comments.models import Comment
 from comments.forms import CommentForm
 from accounts.models import UserAccount
-
-from .models import Product, Category, Variety
 
 
 def all_products(request):
@@ -148,3 +148,14 @@ def product_detail(request, product_id):
     }
 
     return render(request, 'products/product_detail.html', context)
+
+
+def add_product(request):
+    """ Add a product to the site """
+    form = ProductForm()
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
