ba09290c7a4bfc991539749e8b2a7c14d9447c17
fayskerritt <fayskerritt1@gmail.com>
2021-04-28 23:49:12 +0000

    Finished off add product functionality and tested with and without image

--- products/views.py
+++ products/views.py
@@ -152,7 +152,19 @@ def product_detail(request, product_id):
 
 def add_product(request):
     """ Add a product to the site """
-    form = ProductForm()
+
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Product added!')
+            return redirect(reverse('add_product'))
+        else:
+            messages.error(
+                request, 'Failed to add product. Please ensure form is valid')
+    else:
+        form = ProductForm()
+
     template = 'products/add_product.html'
     context = {
         'form': form,
