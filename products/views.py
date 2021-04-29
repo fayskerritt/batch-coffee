56fd25b68b8ea0fad27dc67beda4ed9c00cee24f
fayskerritt <fayskerritt1@gmail.com>
2021-04-29 00:22:37 +0000

    Added delete view and url and added edit and delete buttons for superusers in shop.html and product detail pages

--- products/views.py
+++ products/views.py
@@ -197,3 +197,12 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    """ Delete a product from the site """
+
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product deleted!')
+    return redirect(reverse('products'))
