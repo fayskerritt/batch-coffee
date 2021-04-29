3b117278ad01b16646dc6a2aaceaf597e0a8af95
fayskerritt <fayskerritt1@gmail.com>
2021-04-29 01:39:36 +0000

    Removed secret key and set debug to only be true if there is a variable called 'DEVELOPMENT'

--- batch_coffee/settings.py
+++ batch_coffee/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%n+zaz39$#9d9jjrfsgztqhyji4tyo&ww7kw&*j)#8^!jilq@h'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['batch-coffee.herokuapp.com', 'localhost']
 
